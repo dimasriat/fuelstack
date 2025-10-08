1dbdf079267a5f33c46a63a5b9fc1aa3a88d2e0d
Dimas <dimasriat@gmail.com>
2025-10-08 15:06:31 +0700

    chore: add config to keeper

--- packages/keeper/src/config/chains.ts
+++ packages/keeper/src/config/chains.ts
@@ -17,29 +17,20 @@ export const SOURCE_CHAINS: Record<number, ChainConfig> = {
     chainId: 421614,
     name: 'Arbitrum Sepolia',
     rpcUrl: 'https://arbitrum-sepolia-rpc.publicnode.com',
-    openGate: '0x...', // TODO: Set actual OpenGate contract address after deployment
+    openGate: '0xc2287A4DF839A6ca7B97202178914208BD1B18E2', // TODO: Set actual OpenGate contract address after deployment
     nativeCurrency: 'ETH',
     viemChain: arbitrumSepolia
   },
-  // Base Sepolia  
-  84532: {
-    chainId: 84532,
-    name: 'Base Sepolia',
-    rpcUrl: 'https://base-sepolia-rpc.publicnode.com',
-    openGate: '0x...', // TODO: Set actual OpenGate contract address after deployment
-    nativeCurrency: 'ETH',
-    viemChain: baseSepolia
-  }
 };
 
 // Destination chain with FillGate contract
 export const DESTINATION_CHAIN: ChainConfig = {
-  chainId: 421614, // Using Arbitrum Sepolia as destination
-  name: 'Arbitrum Sepolia',
-  rpcUrl: 'https://arbitrum-sepolia-rpc.publicnode.com',
-  fillGate: '0x...', // TODO: Set actual FillGate contract address after deployment
+  chainId: 84532,
+  name: 'Base Sepolia',
+  rpcUrl: 'https://base-sepolia-rpc.publicnode.com',
+  fillGate: '0xe151FE7360B77973133E2d3D1A0B47A386Ba43Cf', // TODO: Set actual OpenGate contract address after deployment
   nativeCurrency: 'ETH',
-  viemChain: arbitrumSepolia
+  viemChain: baseSepolia
 };
 
 // Helper functions
